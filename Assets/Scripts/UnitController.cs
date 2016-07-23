0c2a2062733bc3be7a3d154fa2c9fdadd898f350
Snow, Dylan (NZ) <Dylan.Snow@cubic.com>
2016-07-23 14:36:56 +1200

    Dylans latest version 1430 hours

---
+++ Assets/Scripts/UnitController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class  UnitController : MonoBehaviour {
+    public int health = 100;
+    //private MeshRenderer meshRenderer;
+
+    private MeshCollider meshCollider;
+
+	// Use this for initialization
+	void Start () {
+        //meshRenderer = this.GetComponentInChildren<MeshRenderer>();
+        meshCollider = this.GetComponent<MeshCollider>();
+        
+	}
+	
+    public void ChangeHealth(int change)
+    {
+        health += change;
+
+    }
+
+    private void CheckHealth()
+    {
+        if(health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        //meshRenderer.enabled = !meshRenderer.enabled;
+        CheckHealth();
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Projectile"))
+        {
+            ChangeHealth(-100);
+        }
+    }
+}
---
+++ Assets/Scripts/UnitSpawnController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UnitSpawnController : MonoBehaviour
+{
+    public bool randomlySpawnUnits;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Returns a vector of locations that units may spawn from
+    List<Vector3> GetRandomSpawnLocations(float innerRadius, float outerRadius, int numberOfSpawnLocations)
+    {
+        // Make sure units won't be spawned outside the right area
+        if (GameObject.FindGameObjectWithTag("Landscape").transform.localScale.x < outerRadius ||
+            GameObject.FindGameObjectWithTag("Landscape").transform.localScale.z < outerRadius ||
+            outerRadius < innerRadius)
+        {
+            Debug.Log("Cannot spawn units from locations outside of the Landscape object, or have innerRadius larger than outerRadius");
+            return null;
+        }
+        List<Vector3> randomSpawnLocations = new List<Vector3>();
+        for (int i = 0; i < numberOfSpawnLocations; i++)
+        {
+            Vector3 randomSpawnLocation = new Vector3(Random.Range(innerRadius, outerRadius), 0, Random.Range(innerRadius, outerRadius));
+            // Move spawn locations to one metre above the ground
+            RaycastHit hitInfo;
+            //if (Physics.Raycast(randomSpawnLocation, transform.up, out hitInfo)
+            //numberOfSpawnLocations.Add();
+        }
+        return randomSpawnLocations;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
